05607d9c02d979f7e3ee0e6c4facc5a5e099db70
Tom Scott <tomscott85@gmail.com>
2017-01-18 23:08:15 +1100

    count through each number at different speeds

--- prototype01/main/main.ino
+++ prototype01/main/main.ino
@@ -10,68 +10,113 @@ void setup()
  PORTB=0x00; // make pins 8-13 LOWs
 }
 
-void loop() {
+void loop()
+{
   removeDecimal(); // turn off decimal place once as it will never need to be on.
+  int dig1value;
+  int dig2value;
+  int dig3value;
+  int dig4value;
   int i;
   int ticker = -5;
 
   for (i=0;i<LOOPTIME;i++)
   {
     ticker = ticker + 5; // test ticker += 5;
-    int dig1value = digitOneScript(ticker);
-    int dig2value = digitTwoScript(ticker);
-    // TODO: refactor this code and figure out how to display two different
-    // digits, logic of setting the numbers may need to be changed back to the
-    // old style.
-    if (dig1value > 0) {
-      digitalWrite(DIG[0], HIGH);
-    } else {
-      digitalWrite(DIG[0], LOW);
-    }
-    if (dig2value > 0) {
-      digitalWrite(DIG[1], HIGH);
-    } else {
-      digitalWrite(DIG[1], LOW);
-    }
-
-    delay(5);
+    dig1value = digitOneScript(ticker);
+    dig2value = digitTwoScript(ticker);
+    dig3value = digitThreeScript(ticker);
+    dig4value = digitFourScript(ticker);
+    
+    activateDigit(0);
+    displayDigit(dig1value);
+    delay(1);
+    activateDigit(1);
+    displayDigit(dig2value);
+    delay(1);
+    activateDigit(2);
+    displayDigit(dig3value);
+    delay(1);
+    activateDigit(3);
+    displayDigit(dig4value);
+    delay(1);
   }
-  //reset all digits...
+  delay(1);
+
+}
+
+void activateDigit(int digit) {
+  int i;
+  for (i=0;i<4;i++)
+  {
+    if (i == digit) {
+      digitalWrite(DIG[i], HIGH);
+    } else {
+      digitalWrite(DIG[i], LOW);
+    }
+  }
 }
 
 int digitOneScript(int tick) {
   int digNum = 0;
   // Script for 1st digit. This will show nothing for 50ms, then 1 for 200ms, then 2 for 200ms etc...
   int startDelay = 50; // start counting 50ms after loop begins
-  int countSpeed = 200; // change number every 200ms
+  int countSpeed = 800; // change number every 200ms
   // normalize tick to speed
   // int normalTick = tick - startDelay
   if (tick < startDelay) {
     return 0;
   } else {
     int result = (tick - startDelay)/countSpeed;
-    if (result < 10) {
+    if (result == 0) {
+      return 10;
+    } else if (result < 10) {
       // digitalWrite(DIG[digNum],HIGH);
       return result;
     } else {
       return result % 10;
     }
   }
-  // HACK: until I can figure how to calculate 1-9 using the tick and countSpeed
-
-  // if(((tick-startDelay)/countSpeed)%1)
 }
 
 int digitTwoScript(int tick) {
-  int digNum = 1;
   int startDelay = 2000;
-  int countSpeed = 400;
+  int countSpeed = 2000;
   if (tick < startDelay) {
     return 0;
   } else {
     int result = (tick - startDelay)/countSpeed;
     if (result < 10) {
-      // digitalWrite(DIG[digNum],HIGH);
+      return result;
+    } else {
+      return result % 10;
+    }
+  }
+}
+
+int digitThreeScript(int tick) {
+  int startDelay = 5000;
+  int countSpeed = 4000;
+  if (tick < startDelay) {
+    return 0;
+  } else {
+    int result = (tick - startDelay)/countSpeed;
+    if (result < 10) {
+      return result;
+    } else {
+      return result % 10;
+    }
+  }
+}
+
+int digitFourScript(int tick) {
+  int startDelay = 0;
+  int countSpeed = 1000;
+  if (tick < startDelay) {
+    return 0;
+  } else {
+    int result = (tick - startDelay)/countSpeed;
+    if (result < 10) {
       return result;
     } else {
       return result % 10;
@@ -84,17 +129,6 @@ void turnOff(int digit) {
   digitalWrite(DIG[digit],LOW);
 }
 
-void countDigit(int digit, int speedDelay) {
-  int n;
-    for (n=0;n<10;n++)
-    {
-      digitalWrite(DIG[digit],HIGH);
-      displayDigit(n);
-      delay(speedDelay);
-      digitalWrite(DIG[digit],LOW);
-    }
-}
-
 void removeDecimal() {
   digitalWrite(9,HIGH);
 }
@@ -104,8 +138,8 @@ void displayDigit(int num)
    switch(num)
    {
      case 0:
-      PORTD=B00000011; // pins 2-7 on
-      digitalWrite(8,HIGH); // turn off pin 8
+      PORTD=B11111111;
+      digitalWrite(8,HIGH);
      break;
      case 1:
       PORTD=B11100111; // only pins 2 and 3 are on
@@ -142,6 +176,10 @@ void displayDigit(int num)
      case 9:
       PORTD=B01000011; // only pins 2,3, 4 and 5 on
       digitalWrite(8,LOW); // segment g on
+     break;
+     case 10: // blank case
+      PORTD=B11111111;
+      digitalWrite(8,HIGH);
      break;
    }
 }
